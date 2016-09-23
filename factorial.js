c19def16eddd9e7d0a1842433b28113c1d4d9f27
ccwisp <armnecro@gmail.com>
2016-09-24 00:09:37 +0400

    Add files via upload

---
+++ factorial.js
@@ -0,0 +1,16 @@
+<script>
+const N= 3;
+	const Factorial = function(x) {
+		if (x===1) {return 1;}
+		return x * Factorial(x-1);
+											}
+											
+											
+											
+											
+console.log(Factorial(N));
+
+
+
+
+</script>
